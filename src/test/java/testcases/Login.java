16de3faee7ff7a8890d7595d8a38d2e8a7e54ad6
unknown <k23.anil@gmail.com>
2025-09-08 00:50:07 +0530

    added login feature branch

---
+++ src/test/java/testcases/Login.java
@@ -0,0 +1,15 @@
+package testcases;
+
+import org.junit.jupiter.api.Test;
+
+public class TestCase1 {
+
+
+    @Test
+    public void test1()
+    {
+        System.out.println("Confirmed Junit Functionality");
+
+
+    }
+}
